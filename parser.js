9cfb3cd2a6b2b6b8be06b98d96a962edb23c40f3
Alexey <payalnik@gmail.com>
2015-06-28 22:02:25 -0700

    GA is now firing after page render; typo fixed

--- parser.js
+++ parser.js
@@ -200,6 +200,15 @@ function draw(content){
 			new Promise(function (){addPosts(drop,toAdd,0);});
 		};
 	}else body.appendChild(genPost(content.posts));
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-0-1', 'auto');
+  ga('send', 'pageview');	
+	
 }
 function addPosts(drop, toAdd, offset){
 	var url = matrix.cfg.srvurl + "/posts?offset="+offset+"&limit="+(toAdd*matrix.cfg.mul);

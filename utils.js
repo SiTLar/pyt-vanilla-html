18fc56d8c1fd81303bd70b50764147f85cc5f234
Ilya Cohn <sitlar@twinspect.net>
2015-12-05 01:48:04 +0000

    now taking ga account from config.json

--- utils.js
+++ utils.js
@@ -207,7 +207,7 @@ _Utils.prototype = {
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,cView.doc,"script","//www.google-analytics.com/analytics.js","ga");
-		ga("create", "UA-0-1", "auto");
+		ga("create", gConfig.ga, "auto");
 		ga("send", "pageview");
 
 		if(parseInt(cView.localStorage.getItem("rt")) ) cView.initRt();

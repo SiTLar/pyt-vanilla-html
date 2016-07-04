4b0ce060e202605d5e07e5c07d414525a928599e
Ilya Cohn <sitlar@twinspect.net>
2016-07-04 23:44:01 +0000

    Wrap screen name de-HTML to avoid multiple conversion

--- common.js
+++ common.js
@@ -56,9 +56,11 @@ _Common.prototype = {
 			return localUser;
 		}
 		var className = "not-my-link";
-		user.screenName = user.screenName.replace(/&/g,"&amp;")
+		(function(screenName){
+		user.screenName = screenName.replace(/&/g,"&amp;")
 			.replace(/</g, "&lt;")
 			.replace(/>/g, "&gt;");
+		})(user.screenName);
 		user.domain = context.domain;
 		if(typeof user.isPrivate !== "undefined")user.isPrivate = JSON.parse(user.isPrivate);
 		if (cView.mode == null) cView.mode = "screen";

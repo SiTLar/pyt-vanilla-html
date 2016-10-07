fad58dcfa54567b1191444f84972d7533a61a8c1
Ilya Cohn <sitlar@twinspect.net>
2016-10-07 22:35:22 +0000

    Correct undefined settings

--- browser.js
+++ browser.js
@@ -10,6 +10,11 @@ window.browserDoc = function(){
 	if (locationPath == "")locationPath = "home";
 	cView.fullPath = locationPath;
 	var arrLocationPath = locationPath.split("/");
+	var settingsNames = require("json!./settings.json");
+	settingsNames.forEach(function(name){
+		var setting =  cView.localStorage.getItem(name);
+		if (setting == "undefined")cView.localStorage.setItem(name, "false");
+	});
 	if(arrLocationPath[0].toLowerCase() != "settings"){
 		cView.search = document.location.search.slice(1);
 		var matchOffset = cView.search.match(/offset=(\d+)/);
